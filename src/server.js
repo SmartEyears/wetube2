7d1236a2098568fa82d55a4eeb4f8bc8e1692f52
AN <dksxogh1940@naver.com>
2021-04-27 23:52:23 +0900

    request and response

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const app = express();
 
 const PORT = 4000;
 
+const handleHome = (req, res) => {
+    return res.send("I still love you! ❤❤");
+}
+
+const handleLogin = (req, res) => {
+    return res.send("Login Here!");
+}
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const hadleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 💨💨💨`);
 
 app.listen(PORT, hadleListening);
