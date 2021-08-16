e5ddb1bc9165d0f9e8446b9f78cc7504eb86891b
AN <dksxogh1940@naver.com>
2021-08-17 00:22:59 +0900

    template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,30 @@
-export const trending = (req, res) => res.render("home");
+const videos = [
+    {
+        title: "첫번째 비디오",
+        rating: 5,
+        comment: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1
+    },
+    {
+        title: "두번째 비디오",
+        rating: 5,
+        comment: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1
+    },
+    {
+        title: "세번째 비디오",
+        rating: 5,
+        comment: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1
+    }
+];
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", videos });
 export const watch = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
