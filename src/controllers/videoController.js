237e91d55817cd5d9ddb0b5f05a8846126766cf6
AN <dksxogh1940@naver.com>
2021-08-18 23:59:13 +0900

    href videos

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,4 @@
-const videos = [
+let videos = [
     {
         title: "첫번째 비디오",
         rating: 5,
@@ -13,7 +13,7 @@ const videos = [
         comment: 2,
         createdAt: "2 minutes ago",
         views: 59,
-        id: 1
+        id: 2
     },
     {
         title: "세번째 비디오",
@@ -21,11 +21,17 @@ const videos = [
         comment: 2,
         createdAt: "2 minutes ago",
         views: 59,
-        id: 1
+        id: 3
     }
 ];
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", videos });
-export const watch = (req, res) => res.render("watch");
+export const trending = (req, res) => {
+    return res.render("home", { pageTitle: "Home", videos })
+};
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("watch", { pageTitle: `Watching ${video.title}` });
+}
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
